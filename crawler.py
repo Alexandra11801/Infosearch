ebd46a4deb73f806f2c7d3d1720871884a786166
Alexandra <alexaver11801@gmail.com>
2022-03-10 16:33:35 +0300

    Add lemmatizer

--- crawler.py
+++ crawler.py
@@ -3,19 +3,19 @@ import urllib.request
 import urllib.error
 
 path = 'https://www.labirint.ru/books/'
-docPath = 'documents\\doc'
-indexFile = open('index.txt', 'a')
+doc_path = 'documents\\doc'
+index_file = open('index.txt', 'a')
 i = 1
 while i <= 200:
     index = random.randint(1,500000)
     url = path+str(index)+'/'
     try:
         page = urllib.request.urlopen(url)
-        doc = open(docPath+str(i)+'.txt', 'w', encoding='utf-8')
+        doc = open(doc_path+str(i)+'.txt', 'w', encoding='utf-8')
         doc.write(page.read().decode('utf8'))
         doc.close()
-        indexFile.write('{}. {}\n'.format(i, url))
+        index_file.write('{}. {}\n'.format(i, url))
         i += 1
     except (urllib.error.HTTPError, ConnectionResetError):
         continue
-indexFile.close()
+index_file.close()
---
+++ tokenizator.py
@@ -0,0 +1,30 @@
+import pymorphy2
+import re
+from nltk.corpus import stopwords
+
+doc_path = 'documents\\doc'
+tokens_file = open('tokens.txt', 'a', encoding='utf-8')
+tokens = set()
+lemmas = dict()
+stops = stopwords.words('russian')
+morph = pymorphy2.MorphAnalyzer()
+for i in range(1, 201):
+    doc = open(doc_path+str(i)+'.txt', 'r', encoding='utf-8')
+    content = doc.read()
+    words = [w for w in re.findall(r'[а-я]+', content.lower()) if w not in stops and len(w) > 2]
+    for word in words:
+        token = morph.parse(word)[0].normal_form
+        if token not in lemmas.keys():
+            tokens.add(token)
+            lemmas[token] = set()
+            tokens_file.write('{}\n'.format(token))
+        lemmas[token].add(word)
+    doc.close()
+tokens_file.close()
+lemmas_file = open('lemmas.txt', 'a', encoding='utf-8')
+for token in tokens:
+    lemmas_file.write('{}:'.format(token))
+    for t in lemmas[token]:
+        lemmas_file.write(' {}'.format(t))
+    lemmas_file.write('\n')
+lemmas_file.close()
